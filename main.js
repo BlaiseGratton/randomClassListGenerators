c6b726dfa3143bd9715580c55a1b4f0a13c79fd1
BlaiseGratton <blaisegratton@gmail.com>
2014-10-17 16:25:02 -0500

    Semi-functioning random pair generator (some double names appear)

--- main.js
+++ main.js
@@ -3,21 +3,29 @@ document.addEventListener('DOMContentLoaded', function(){
 
     $button.addEventListener('click', function(){
 	var $dropdown = document.getElementById('dropdown').value;
-	    if ($dropdown === "random_student"){
-		var $target = document.querySelector('.target_random_student');
-	    	var docFragment = printRandomStudent();
-		$target.appendChild(docFragment);
-	    } else 
-	    if ($dropdown === "neighbor_pairing"){
-		var $target = document.querySelector('.target_random_pair');
-		var docFragment = printRandomPair();
-		$target.appendChild(docFragment);
-	    } else
-	    if ($dropdown === "teams_of_three"){
-		var $target = document.querySelector('.target_team_of_three');
-		var docFragment = printTeamOfThree();
-		$target.appendChild(docFragment);
-	    }
+	if ($dropdown === "random_student"){
+	    var $target = document.querySelector('.target_random_student');
+	    var docFragment = printRandomStudent();
+	    $target.appendChild(docFragment);
+	} else 
+	if ($dropdown === "neighbor_pairing"){
+	    var $target = document.querySelector('.target_random_pair');
+	    var docFragment = printRandomPair();
+	    $target.appendChild(docFragment);
+	} else
+	if ($dropdown === "teams_of_three"){
+	    var $target = document.querySelector('.target_team_of_three');
+	    var docFragment = printTeamOfThree();
+	    $target.appendChild(docFragment);
+	} else
+	if ($dropdown === "random_pairing"){
+	    var $target = document.querySelector('.target_randomly_paired');
+	    var docFragment = printRandomlyPaired();
+	    $target.appendChild(docFragment);
+	} else
+	if ($dropdown === "random_n_pairing"){
+	    
+	}
     });
 });
 
@@ -46,7 +54,7 @@ function printRandomPair (){
     
     var randomNumber = Math.random();
     var random_index = (Math.round(randomNumber*(classList.length-2)));
-    var random_index2 = random_index++
+    var random_index2 = random_index+1
     if (classList.length < 2){
 	alert("Not enough students remaining!");
 	return;
@@ -65,18 +73,51 @@ function printTeamOfThree(){
 
     var randomNumber = Math.random();
     var random_index = (Math.round(randomNumber*(classList.length-3)));
-    var random_index2 = random_index++;
-    var random_index3 = random_index2++;
+    var random_index2 = random_index+1;
+    var random_index3 = random_index2+1;
     if (classList.length < 3) {
 	alert("Not enough students remaining!");
 	return;
-    }
+    } 
     var $li = document.createElement('li');
     var $randomTriplet = document.createTextNode(classList[random_index] + ", " + classList[random_index2] + ", & " + classList[random_index3]);
     $li.appendChild($randomTriplet);
     docFragment.appendChild($li);
-    classList.slice([random_index], 3);
     console.log(random_index, random_index2, random_index3);
-    
+    classList.splice([random_index], 3);
     return docFragment;
 }
+
+function printRandomlyPaired (){
+    var docFragment = document.createDocumentFragment();
+    
+    if (classList.length < 2) {
+	alert("Not enough students remaining!");
+	return;
+    }
+    var randomNumber = Math.random();
+    var random_index = (Math.round(randomNumber * (classList.length-1)));
+    var random_index2 = (Math.round(Math.random() * (classList.length-1)));
+    console.log(random_index, random_index2);
+    if (random_index === random_index2) {
+	if (random_index === 0, random_index2 === 0) {
+	    random_index = random_index + 1;
+	} else { if (random_index === 1, random_index2 === 0) {
+	    random_index = random_index - 1;
+	}
+    }
+    }
+    var $li = document.createElement('li');
+    var $randomlyPaired = document.createTextNode(classList[random_index] + " & " + classList[random_index2]);
+    $li.appendChild($randomlyPaired);
+    docFragment.appendChild($li);
+    console.log(random_index, random_index2);
+    classList.splice([random_index], 1);
+    if (random_index < random_index2) {
+	random_index2 = random_index2 - 1;
+    } 
+    classList.splice([random_index2], 1);
+    return docFragment;
+}
+
+
